17a76b281cb77461d2a3152198ce1cb2b5624ede
chaechae <hhh00613@naver.com>
2023-06-28 21:13:11 +0900

    명명규칙 수정

--- src/test/java/com/kia/backend/service/MakeStringServiceTest.java
+++ src/test/java/com/kia/backend/service/MakeStringServiceTest.java
@@ -24,10 +24,14 @@ class MakeStringServiceTest {
     }
 
     @Test
-    @DisplayName("문자열 생성 테스트 - 문자열 전처리 테스트 (replace/중복제거/오름차순정렬)")
+    @DisplayName("문자열 생성 테스트 - 문자열 전처리 테스트 (숫자,알파벳만 남기고 제거/중복제거/오름차순정렬)")
     void givenTestData_whenReplaceDistinctSortCharStream_thenPreProcessCheck() {
         // given
-        String targetString = "AaBCDdefghIilmtv123";
+        String targetString = "A23****<12222aaaaBCDdddddefghIilmtv123";
+        // when
+        Stream<Character> characterStream = makeStringService.replaceDistinctSortCharStream(targetString);
+        // then
+
     }
 
     @Test
@@ -40,6 +44,7 @@ class MakeStringServiceTest {
         String result = makeStringService.alphabetNumericCrossSort(targetCharStream);
         // then
         String expectedResult = "Aa1B2C4DdefghIilmtv";
-        assertThat(expectedResult).isEqualTo(targetString);
+        // 희망하는 결과 값과 실제 연산되여 나온 결과 값이 동일해야 함
+        assertThat(expectedResult).isEqualTo(result);
     }
 }
